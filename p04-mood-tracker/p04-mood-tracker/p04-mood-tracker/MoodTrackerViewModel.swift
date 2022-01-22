9774802c782064ce13afd61f96cf3f850d768b23
Peter Wi <peter.h.wi96@gmail.com>
2022-01-22 17:53:27 -0500

    Applied MVVM. Needs to make it simple on View.

--- p04-mood-tracker/p04-mood-tracker/p04-mood-tracker/MoodTrackerViewModel.swift
+++ p04-mood-tracker/p04-mood-tracker/p04-mood-tracker/MoodTrackerViewModel.swift
@@ -10,22 +10,42 @@ import SwiftUI
 
 final class MoodTrackerViewModel: ObservableObject {
     @Published private var moods: [Mood] = []
+    @Published var selectedStrength = 1
+    @Published var myMood = ""
+    @Published var myColor = Color.cyan
+    @Published var isShort = false
     
-    func addMood(curMood: String, curStrength: Int, curColor: Color) -> Int {
-        let curDate = Date()
-        
-        if (curMood == "") {
-            return 1
+    func addMood() {
+        if (myMood == "") {
+            return
         }
         
-        let newMood = Mood(mood: curMood, strength: curStrength+1, color: curColor, timestamp: curDate)
+        let curDate = Date()
+        
+        let newMood = Mood(mood: myMood, strength: selectedStrength+1, color: myColor, timestamp: curDate)
         moods.append(newMood)
         
-        return 0
+        resetInput()
     }
     
-    func getMoods() -> [Mood] {
-        return moods
+    func resetInput() {
+        selectedStrength = 1
+        myMood = ""
+    }
+    
+    func getMoods() -> ArraySlice<Mood> {
+        if (isShort) {
+            return getTopThreeMoods()
+        }
+        return moods[0..<moods.count]
+    }
+    
+    func getTopThreeMoods() -> ArraySlice<Mood> {
+        let n = moods.count
+        if (n>=3) {
+            return moods[n-3..<n]
+        }
+        return moods[0..<n]
     }
 }
 
